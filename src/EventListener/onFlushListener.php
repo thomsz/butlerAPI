159433f1cc2f347776c16798d9eba1332d6d71ac
Tom Segev <tom@golaender.com>
2020-06-28 11:05:09 +0200

    Add /update tracker

--- src/EventListener/onFlushListener.php
+++ src/EventListener/onFlushListener.php
@@ -20,7 +20,32 @@ class FlushListener
         foreach ($unitOfWork->getScheduledEntityDeletions() as $entity) {
         }
 
-        foreach ($unitOfWork->getScheduledEntityUpdates() as $updatedEntity) {
+        foreach ($unitOfWork->getScheduledEntityUpdates() as $entity) {
+
+            if ($entity instanceof Customer) {
+
+                $changeSet = $unitOfWork->getEntityChangeSet($entity);
+                $trackChange = [];
+
+                foreach ($changeSet as $change) {
+                    $previousValueForField = array_key_exists(0, $change) ? $change[0] : null;
+                    $newValueForField = array_key_exists(1, $change) ? $change[1] : null;
+
+                    if ($previousValueForField != $newValueForField) {
+                        $trackChange[] = ['from' => $previousValueForField, 'to' => $newValueForField];
+                    }
+                }
+
+                $tracker = new Tracker();
+                $tracker->setUserID($entity->getId());
+                $tracker->setChange('update');
+                $tracker->setContent($trackChange);
+                $tracker->setTime(new \Datetime());
+
+                $entityManager->persist($tracker);
+                $metaData = $entityManager->getClassMetadata('App\Entity\tracker');
+                $unitOfWork->computeChangeSet($metaData, $tracker);
+            }
         }
     }
 }
